a426dbffad9df45d5c1ae2e1d8241a71824495b3
CattacoCattaco <cattacoiscool@gmail.com>
2026-08-14 18:27:39 -0500

    Add Orders
    Added orders list, which has a new order added at a constant time gap. If it has at least as many orders as the max customer count, the order is added to a separate delayed customers list.

--- arepitas-de-padres/scripts/game.gd
+++ arepitas-de-padres/scripts/game.gd
@@ -1,11 +1,31 @@
+class_name Game
 extends Node2D
+
+const MAX_CUSTOMER_COUNT: int = 4
+const NEW_CUSTOMER_COOL_DOWN_SECS: float = 30
+
+var delayed_customers: Array[Order]
+var customer_orders: Array[Order]
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	add_order()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func add_order() -> void:
+	if customer_orders.size() == MAX_CUSTOMER_COUNT:
+		delayed_customers.append(Order.new(1))
+	else:
+		customer_orders.append(Order.new(1))
+	
+	var timer: SceneTreeTimer = get_tree().create_timer(NEW_CUSTOMER_COOL_DOWN_SECS)
+	timer.timeout.connect(add_order)
+	print_orders()
+
+
+func print_orders() -> void:
+	print("Orders:")
+	for customer_order in customer_orders:
+		print("Order: %s arepas" % customer_order.arepa_count)
+	print("")
---
+++ arepitas-de-padres/scripts/order.gd
@@ -0,0 +1,8 @@
+class_name Order
+extends RefCounted
+
+var arepa_count: int
+
+
+func _init(p_arepa_count: int) -> void:
+	arepa_count = p_arepa_count
