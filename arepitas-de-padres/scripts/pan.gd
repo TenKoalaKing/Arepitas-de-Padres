248685e545f47b6f86dc22bf0215aa903f89b860
Samuel Naranjo <samuel.naranjo.eng@gmail.com>
2026-08-15 13:25:29 -0500

    feat(core): [REQ-001] implement the arepa and pan game logic
    - Add a countdown timer for each side of the arepas
    - Implement burn animation one of the sides hits 0 of time to cook left
    - Implment flip logic to change the sides of the cooking

---
+++ arepitas-de-padres/scripts/pan.gd
@@ -0,0 +1,55 @@
+extends Node2D
+@export var target_arepa: Node
+
+var burning_limit: float = 12.0
+
+# Pan Down counter
+# @trace SREQ-001A @
+@onready var timer_label = $CanvasLayer/TimerText
+
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+# @trace SREQ-001A @
+func _process(delta: float) -> void:
+	
+	var arepa_timer = target_arepa.get_node_or_null("Timer")
+	
+	if arepa_timer:
+		# Here use some sort of import from the burning level to define
+		# The actual time left 
+		if target_arepa.is_side_A:
+			var current_limit_state = burning_limit - (target_arepa.burning_level_side_A / 10)
+			timer_label.text = str(current_limit_state)
+		else:
+			var current_limit_state = burning_limit - (target_arepa.burning_level_side_B / 10)
+			timer_label.text = str(current_limit_state)
+	
+	if target_arepa.is_arepa_burned:
+		timer_label.text = "This arepa is overcoked"
+		
+		
+var func_clock_die
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("Arepa tocuh pan!!!!")
+	if area.has_method("on_pan_touch"):
+		area.on_pan_touch(burning_limit)
+		
+
+
+# @trace SREQ-001C @ 
+# Handles arepa place over a pan
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				print('This should flip the pan')
+				target_arepa.on_pan_flip(burning_limit)
+		
---
+++ docs/ARP-SREQ-001C_Drag_The_Arepa.md
@@ -0,0 +1,34 @@
+---
+Project:
+Status: 4-Done
+Priority:
+Description:
+---
+
+# Connections
+
+| Type                | Route                                                                                                 |
+| :------------------ | :---------------------------------------------------------------------------------------------------- |
+| **📓 Requirements** | [ARP-SREQ-001B\_Pan\_Flip](ARP-SREQ-001B_Pan_Flip.md)                                                 |
+| **📂 Other**        | [area\_2d](../arepitas-de-padres/scripts/area_2d.gd)<br />[pan](../arepitas-de-padres/scripts/pan.gd) |
+
+# Acceptance Criteria
+
+> [!success] **Scenario:** Arepa Drag
+> `Precondition - Action - Outcome`
+>
+> **Given** a listener is set , **When** the user drags the arepa , **Then**
+>
+> 1. Drag seamlessly frame by frame to the pan
+
+***
+
+## References
+
+* **Implements:** @trace @
+* **Depends On:** @trace @
+* **Parent:** @trace REQ-001 @
+
+***
+
+## PKB References
