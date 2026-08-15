eb849461c7ac4b846872098dafdc69495731815d
CattacoCattaco <cattacoiscool@gmail.com>
2026-08-15 09:39:37 -0500

    Add Start Menu
    Added start menu

--- arepitas-de-padres/scripts/game.gd
+++ arepitas-de-padres/scripts/game.gd
@@ -6,12 +6,13 @@ const MIN_CUSTOMER_COOL_DOWN_SECS: float = 20
 const MAX_CUSTOMER_COOL_DOWN_SECS: float = 50
 const FADE_DURATION_SECS: float = 0.5
 
+@export var start_menu: Control
 @export var customer_screen: Sprite2D
 @export var interior: Sprite2D
 
-@export var interior_camera: ScrollCamera
-
+@export var start_button: Button
 @export var shop_button: Button
+@export var interior_camera: ScrollCamera
 
 @export var screen_overlay: ColorRect
 
@@ -21,11 +22,18 @@ var customer_orders: Array[Order]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	customer_screen.show()
+	start_menu.show()
+	customer_screen.hide()
 	interior.hide()
+	
+	start_button.pressed.connect(go_to_customer_screen)
 	shop_button.pressed.connect(go_to_shop)
+	
+	start_menu.custom_minimum_size = get_viewport_rect().size
 	screen_overlay.custom_minimum_size = get_viewport_rect().size
+	
 	screen_overlay.color = Color(0, 0, 0, 0)
+	
 	add_order()
 
 
@@ -57,6 +65,7 @@ func print_orders() -> void:
 
 func go_to_shop() -> void:
 	await start_scene_transition()
+	start_menu.hide()
 	customer_screen.hide()
 	interior.show()
 	interior_camera.enabled = true
@@ -66,6 +75,7 @@ func go_to_shop() -> void:
 
 func go_to_customer_screen() -> void:
 	await start_scene_transition()
+	start_menu.hide()
 	customer_screen.show()
 	interior.hide()
 	interior_camera.enabled = false
