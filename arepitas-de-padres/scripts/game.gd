5b05ace24600d57ef1afc5801d2ec67bbf6541df
CattacoCattaco <cattacoiscool@gmail.com>
2026-08-15 07:32:26 -0500

    Add Scene Switching
    Added the ability to switch from the customer screen to the shop interior using the shop button and to go from the interior to the shop using the escape key.

--- arepitas-de-padres/scripts/game.gd
+++ arepitas-de-padres/scripts/game.gd
@@ -5,13 +5,30 @@ const MAX_CUSTOMER_COUNT: int = 4
 const MIN_CUSTOMER_COOL_DOWN_SECS: float = 20
 const MAX_CUSTOMER_COOL_DOWN_SECS: float = 50
 
+@export var customer_screen: Sprite2D
+@export var interior: Sprite2D
+
+@export var interior_camera: Camera2D
+
+@export var shop_button: Button
+
 var delayed_customers: Array[Order]
 var customer_orders: Array[Order]
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	customer_screen.show()
+	interior.hide()
+	shop_button.pressed.connect(go_to_shop)
 	add_order()
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.is_action_pressed("exit"):
+			if interior.visible:
+				go_to_customer_screen()
 
 
 func add_order() -> void:
@@ -31,3 +48,15 @@ func print_orders() -> void:
 	for customer_order in customer_orders:
 		print("Order: %s arepas" % customer_order.arepa_count)
 	print("")
+
+
+func go_to_shop() -> void:
+	customer_screen.hide()
+	interior.show()
+	interior_camera.enabled = true
+
+
+func go_to_customer_screen() -> void:
+	customer_screen.show()
+	interior.hide()
+	interior_camera.enabled = false
