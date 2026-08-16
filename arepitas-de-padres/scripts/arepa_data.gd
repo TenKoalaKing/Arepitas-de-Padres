e54e2ec8e3f17537fe2f5a159e56795d91fc9cc3
CattacoCattaco <cattacoiscool@gmail.com>
2026-08-15 23:23:59 -0500

    Add Initializer to ArepaData
    Gave ArepaData class an _init function.

--- arepitas-de-padres/scripts/arepa_data.gd
+++ arepitas-de-padres/scripts/arepa_data.gd
@@ -7,7 +7,13 @@ enum Filling {
 	CHICKEN,
 	BACON,
 	CORN,
+	NONE
 }
 
 var filling_1: Filling
 var filling_2: Filling
+
+
+func _init(p_filling_1: Filling = Filling.NONE, p_filling_2: Filling = Filling.NONE) -> void:
+	filling_1 = p_filling_1
+	filling_2 = p_filling_2
