2bdb068d2cc6bc597d2cccb6101f921152ee910b
TenKoalaKing <bennettheinzelman@gmail.com>
2026-08-16 08:16:34 -0500

    working

--- arepitas-de-padres/scripts/area_2d.gd
+++ arepitas-de-padres/scripts/area_2d.gd
@@ -113,7 +113,7 @@ func _on_timer_timeout() -> void:
 
 
 # @trace SREQ-001C @
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_input_event(viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
